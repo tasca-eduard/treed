af3cb9ac33450071602b0f8e9d05ec1e7432d89c
Eduard Cristian Tasca <eduardcristiantasca@gmail.com>
2022-12-28 16:18:02 +0200

    Added html and stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <div className="container neutral">
+        <h1>TreEd</h1>
+        <p>An approach for creating system trees.</p>
+      </div>
+      <div className="container">
+        <h2>Basic directory tree</h2>
+        <ul className="tree">
+          <li>
+            First file
+          </li>
+          <li>
+            Second file
+          </li>
+          <li>
+            <button>
+              <FontAwesomeIcon icon={faMinus} />
+              <span>First directory</span>
+            </button>
+            <ul>
+              <li>
+                First file of directory
+              </li>
+              <li>
+                <button>
+                  <FontAwesomeIcon icon={faPlus} />
+                  <span>First subdirectory</span>
+                </button>
+              </li>
+            </ul>
+          </li>
+          <li>
+            Third file
+          </li>
+          <li>
+            Fourth file
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }
 
